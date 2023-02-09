4d73a91793a132646a4c926e08796a1b0bf0a1ba
Idan Fischer <Idanfischer5@gmail.com>
2023-02-09 11:43:41 -0500

    signup form now connected to api

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -10,12 +10,23 @@ export default function SignupForm() {
   const handleSubmit = (e) => {
     e.preventDefault()
     // make a post request to the API with the form data
+    fetch('http://127.0.0.1:5002/signup', {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email, password})
+    })
     // create a new user in the database
-    // then...
     // 1. do something with the new user
-    setUser({ email, password })
-    // 2. redirect to the content page
-    navigate('/secret')
+    // then...
+    .then(res => res.json())
+    .then(res => {
+      setUser(res.user)
+      // 2. redirect to the content page
+      navigate('/secret')
+    })
+    .catch(err => alert(err.message))
   }
   return (
     <>
